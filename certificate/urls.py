a093a683d3b24b0528736cb71a044b7df28c0827
gupta-sanchit <sanchitgupta1802@gmail.com>
2020-10-09 01:41:42 +0530

    - Code Clean Up

--- certificate/urls.py
+++ certificate/urls.py
@@ -1,9 +1,7 @@
 from django.conf.urls import url
 from api import views
-from django.urls import path
 
 urlpatterns = [
-    url(r'^participant/', views.participant_list),
     url(r'^certificate/(?P<email>[\s\S]*)$', views.certificate)
 ]
 
