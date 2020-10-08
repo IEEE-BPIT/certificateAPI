3d663d349ca120af36154253ec495d71af09c966
gupta-sanchit <sanchitgupta1802@gmail.com>
2020-10-08 23:38:13 +0530

    - updated db

--- add_participants.py
+++ add_participants.py
@@ -1,13 +1,14 @@
-import requests 
+import requests
 import json
 import pandas as pd
 
-participants =  pd.read_csv('C:/Users/rajat/gen-certificate/participant_list.csv')
+participants = pd.read_csv('participant_list.csv')
 URL = "http://localhost:8000/participant/"
 for i in range(len(participants)):
     data = {'name': participants.iloc[i]['name'],
             'email': participants.iloc[i]['email'],
-            'gender':  participants.iloc[i]['gender'],
-           }
+            'gender': participants.iloc[i]['gender'],
+            }
     data_json = json.dumps(data)
-    r = requests.post(URL,data=data_json,verify=False)
+    r = requests.post(URL, data=data_json, verify=False)
+print("done")
---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/sanchit/Documents/certificateAPI/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
